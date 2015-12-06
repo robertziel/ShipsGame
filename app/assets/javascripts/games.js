4ba278824a25601c742bef87a18fc5df01abed96
robo-hobo <robertzielinski@dobrapogoda24.pl>
2015-12-06 21:17:56 +0100

    Ship positioning

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -6,18 +6,22 @@
     this.userboard = userData;
 
     this.Ship = [0,0,0,0,0,0,0,0,0,0];
-
     this.shipRotate = function(num){
-      if (this.Ship[num] === 0) {
-        this.Ship[num] = 1;
-      }
-      else {
-        this.Ship[num] = 0;
-      }
+      if (this.Ship[num] === 0) {this.Ship[num] = 1;} else {this.Ship[num] = 0;}
     };
-    
+
+    this.position = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
+    this.changePosition = function(num){
+      this.position[num][0] = angular.element( document.querySelector( '.Ship' + num ) ).prop('offsetLeft');
+      this.position[num][1] = angular.element( document.querySelector( '.Ship' + num ) ).prop('offsetTop');
+    }
+
   });
 
 })();
+
+
+
+
 
 var userData = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
