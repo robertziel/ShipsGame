af6a750a1cd7241ec43a21be37d61a264b937a85
robo-hobo <robertzielinski@dobrapogoda24.pl>
2015-12-06 22:42:03 +0100

    Absolute element position tracking

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -12,9 +12,24 @@
 
     this.position = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
     this.changePosition = function(num){
-      this.position[num][0] = angular.element( document.querySelector( '.Ship' + num ) ).prop('offsetLeft');
-      this.position[num][1] = angular.element( document.querySelector( '.Ship' + num ) ).prop('offsetTop');
-    }
+      var offsetHash = offset(angular.element( document.querySelector( '.Ship' + num ) ));
+      this.position[num][0] = offsetHash.left;
+      this.position[num][1] = offsetHash.top;
+    };
+
+
+    this.fields = arr;
+
+    this.checkPosition = function(){
+      var elementsFields = angular.element( document.querySelector('.gameBoard' )).children().children().children();
+      for (var x = 0; x < 10; x++){
+        for (var y = 0; y < 10; y++){
+          var offsetHash = offset(angular.element(elementsFields[(y * 10) + x]));
+          this.fields[x][y][0] = offsetHash.left;
+          this.fields[x][y][1] = offsetHash.top;
+        }
+      }
+    };
 
   });
 
@@ -22,6 +37,30 @@
 
 
 
-
+//initialize 3D array
+arr = [];
+for(var x = 0; x < 10; x++){
+    arr[x] = [];
+    for(var y = 0; y < 10; y++){
+        arr[x][y] = [0,0];
+    }
+}
+//initialize 3D array
 
 var userData = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
+
+
+
+//ABSOLUTE ELEMENT POSITION
+function offset(elm) {
+  try {return elm.offset();} catch(e) {}
+  var rawDom = elm[0];
+  var _x = 0;
+  var _y = 0;
+  var body = document.documentElement || document.body;
+  var scrollX = window.pageXOffset || body.scrollLeft;
+  var scrollY = window.pageYOffset || body.scrollTop;
+  _x = rawDom.getBoundingClientRect().left + scrollX;
+  _y = rawDom.getBoundingClientRect().top + scrollY;
+  return { left: _x, top: _y };
+}
